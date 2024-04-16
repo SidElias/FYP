0a3ddc748562ec2b491f527384e7315cfb214aea
Sid <83931879+SidElias@users.noreply.github.com>
2024-04-16 15:51:49 +0000

    None

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import qrcode
+from io import BytesIO
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def generate_qr():
+    
+    text = request.args.get('text', '')
+    
+    qr = qrcode.QRCode()
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image()
+
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+
     return render_template("index.html", title="Hello")
